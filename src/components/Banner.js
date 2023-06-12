a27d04c0f669a8e5a09a45df3966870361f13b83
aashay shah <aashayshah28@gmail.com>
2023-06-12 17:01:18 +0530

    added easeIn in banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -88,7 +88,7 @@ const BannerRowBottom = ({ title }) => {
       <motion.div
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
-        transition={{ ease: [0.6, 0.01, -0.05, 0.95], duration: 1, delay: 1 }}
+        transition={{ ease: easeInOut, duration: 1, delay: 1 }}
         className='scroll'
       >
         <motion.span
